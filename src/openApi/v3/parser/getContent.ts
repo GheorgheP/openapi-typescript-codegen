6606bb43fa6a29de9209117160c6a74611876d81
Gheorghe <pisarenco.gh@gmail.com>
2021-05-24 14:28:09 +0300

    Added ld+json suppoprt

--- src/openApi/v3/parser/getContent.ts
+++ src/openApi/v3/parser/getContent.ts
@@ -6,6 +6,9 @@ import type { OpenApiSchema } from '../interfaces/OpenApiSchema';
 export function getContent(openApi: OpenApi, content: Dictionary<OpenApiMediaType>): OpenApiSchema | null {
     /* prettier-ignore */
     return (
+        content['application/ld+json'] &&
+        content['application/ld+json'].schema
+    ) ||  (
         content['application/json-patch+json'] &&
         content['application/json-patch+json'].schema
     ) || (
